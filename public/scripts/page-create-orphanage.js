ed9d7c05476fdf77abab098007b79d944455dd1a
Silvaney <silvaneymara@gmail.com>
2020-10-21 21:11:46 -0300

    Finalizando aplicação

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,96 @@
+//create map
+const map = L.map('mapid').setView([-15.8353097,-47.9006527], 16);
+
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
+//create icon
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAchor: [29,68],
+
+})
+
+let marker;
+
+//create and add marker
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    //remove icon
+
+    marker && map.removeLayer(marker)
+
+    //add icon layer
+    marker = L.marker([lat,lng], { icon })
+    .addTo(map)
+
+})
+
+// adicionar o campo de fotos
+
+function addPhotoField() {
+    
+    // Pegar o container de fotos #images
+    const container = document.querySelector('#images')
+
+    // pegar o container para duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    //realizar o clone da últma image adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    //verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+    if(input.value =="") {
+        return
+    }
+
+    //limpar o campo antes de adicionar ao container de imagens
+    input.value = ""
+
+    //adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2) {
+        //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove();
+}
+
+//seleção do sim ou não
+
+function toggleSelect(event) {
+    //retirar a class .active dos botoes
+    document.querySelectorAll('.button-select button')
+    .forEach( function(button) {
+        button.classList.remove('active')
+    })
+    
+    //colocar a class .active nesse botao clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    //atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+    
+    input.value = button.dataset.value
+}
---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-15.8353097",
+        lng: "-47.9006527",
+        name: "Lar das Meninas 1",
+        description: "1 PPresta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1603138461809-50ff754bc5ac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1573924436756-128cf1f6387a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 08hs às 18hs",
+        open_on_weekends: "1"
+    },
+
+
+
+    {
+        id: 2,
+        lat: "-15.8353097",
+        lng: "-47.9106527",
+        name: "Lar das Meninas 2",
+        description: "2 PPresta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1603138461809-50ff754bc5ac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1573924436756-128cf1f6387a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 08hs às 18hs",
+        open_on_weekends: "0"
+    }
+]
